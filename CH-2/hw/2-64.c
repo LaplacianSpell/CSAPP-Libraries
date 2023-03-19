97ce4a8421926c9eaa0b6e8aa726e07d0b86fc11
LaplacianSpell <pd20@mails.tsinghua.edu.cn>
2023-03-19 14:17:27 +0800

    Added Datalab

--- CH-2/hw/2-64.c
+++ CH-2/hw/2-64.c
@@ -1,10 +1,16 @@
-#include<stdlib.h>
+#include<stdio.h>
 
 /* Return 1 when any even bit of x equal to 1, 0 elsewhere, Assume w = 32*/
 int any_even_one(unsigned x) {
-	return !!(0x55555555 & x) 
+	return !!(0x55555555 & x);
 }
 
+int main() {
+	unsigned num = 0;
+	scanf("%u", &num);
+	printf("%d", any_even_one(num));
+	return 0;
+}
 // 32bits = 4bytes = 0101,0101,...,0101(0x55555555)
-// 
+//
 
